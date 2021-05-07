d3a0b9d844f681ace30a0d920a547aeffe4d756b
rilp98 <63820704+rilp98@users.noreply.github.com>
2021-05-07 19:51:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const container=document.querySelector('.login-label');
+const input=document.querySelector('#password');
+const iconEye=document.querySelector('.icon-eye');
+const iconEyeSlash=document.querySelector('.eyeSlash');
+
+
+iconEye.addEventListener('click',function(){
+
+    /*The .visible class is being placed in the container*/
+    container.classList.toggle('.visible');
+
+    /*It is checked if the class .visible exists*/
+    if(container.classList.contains('.visible')){
+
+        /*If the class .visible exists, the text is displayed*/
+        input.type='text';
+        iconEye.src='./assets/icons/eye.svg';
+    }else{
+        input.type='password';
+        iconEye.src='./assets/icons/eye-off.svg';
+    }
+
+});
+
+
